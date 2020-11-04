98f183dacc4bdebb5cf9ac11dfe23ca344a4c32d
Manish Garg <manishgargpro@gmail.com>
2020-11-04 02:19:37 -0500

    updated resize.py to find percentages

--- resize.py
+++ resize.py
@@ -18,14 +18,34 @@ for r in rows:
     confidences.append(r["face_box_confidence"])
     labels.append(r["expression_label"])
 
+numnotequal = 0
+numundersize = 0
+numveryundersize = 0
+numextremelyundersize = 0
+numcanpad = 0
+
+res = 224
 
 for d, e, f in os.walk(imagepath):
     for fi in f:
         imagelist.append(fi)
+        fim = Image.open(os.path.join(imagepath, fi))
+        w, h = fim.size
+        if w == res - 1:
+            numcanpad += 1
+        # if w < res:
+        #     numundersize += 1
+        #     if w < res/2:
+        #         numveryundersize += 1
+        #         if w < res/4:
+        #             numextremelyundersize += 1
 
+# print(numundersize, len(imagelist), numundersize/len(imagelist))
+# print(numveryundersize, len(imagelist), numveryundersize/len(imagelist))
+# print(numextremelyundersize, len(imagelist),
+#       numextremelyundersize/len(imagelist))
+print(numcanpad, len(imagelist), numcanpad/len(imagelist))
 
-res = 224
-
-im = Image.open(os.path.join(imagepath, imagelist[0]))
-im = im.resize((res, res))
-im.show()
+# im = Image.open(os.path.join(imagepath, imagelist[0]))
+# im = im.resize((res, res))
+# im.show()

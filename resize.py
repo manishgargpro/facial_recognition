f29c81c1818500476fd2ce9e9eae313e3328663b
Manish Garg <manishgargpro@gmail.com>
2020-11-16 10:24:23 -0500

    started on resnet

--- resize.py
+++ resize.py
@@ -6,46 +6,12 @@ from pprint import pprint
 
 labelpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label.csv'
 imagepath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\cropped'
-rows = csv.DictReader(open(labelpath))
-names = []
-confidences = []
-labels = []
-imagelist = []
-
-
-for r in rows:
-    names.append(r["image_name"])
-    confidences.append(r["face_box_confidence"])
-    labels.append(r["expression_label"])
-
-numnotequal = 0
-numundersize = 0
-numveryundersize = 0
-numextremelyundersize = 0
-numcanpad = 0
+destpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\resized'
 
 res = 224
 
 for d, e, f in os.walk(imagepath):
     for fi in f:
-        imagelist.append(fi)
-        fim = Image.open(os.path.join(imagepath, fi))
-        w, h = fim.size
-        if w == res - 1:
-            numcanpad += 1
-        # if w < res:
-        #     numundersize += 1
-        #     if w < res/2:
-        #         numveryundersize += 1
-        #         if w < res/4:
-        #             numextremelyundersize += 1
-
-# print(numundersize, len(imagelist), numundersize/len(imagelist))
-# print(numveryundersize, len(imagelist), numveryundersize/len(imagelist))
-# print(numextremelyundersize, len(imagelist),
-#       numextremelyundersize/len(imagelist))
-print(numcanpad, len(imagelist), numcanpad/len(imagelist))
-
-# im = Image.open(os.path.join(imagepath, imagelist[0]))
-# im = im.resize((res, res))
-# im.show()
+        im = Image.open(os.path.join(imagepath, fi))
+        im = im.resize((res, res))
+        im.save(os.path.join(destpath, fi))
---
+++ resnet.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+from keras.models import Sequential
+from keras.applications import ResNet50, ResNet50V2, ResNet101, ResNet101V2, ResNet152, ResNet152V2
+from keras.layers import Conv2D, BatchNormalization, Dropout
+from keras.layers import AveragePooling2D
+from keras.layers import Dense, Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+# for conversion to one-hot coding
+from keras.utils import to_categorical
+# ======
+# funtion for loading datasets:
+# you have to make changes, if you are not using fashion_mnist dataset
+
+
+labelpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label.csv'
+imagepath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\resized_224'
+# destpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\'
+rows = csv.DictReader(open(labelpath))
+names = []
+confidences = []
+labels = []
+imagelist = []
+
+for r in rows:
+    names.append(r["image_name"])
+    confidences.append(r["face_box_confidence"])
+    labels.append(r["expression_label"])
+
+for d, e, f in os.walk(imagepath):
+    for fi in f:
+        fiarr = np.load(os.path.join(imagepath, fi))
+        imagelist.append(fiarr)
+
+for i in imagelist:
+    print(i)

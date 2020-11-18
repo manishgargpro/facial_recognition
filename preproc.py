628ce65e6b7d48f4b27d6bf941a4db6328d709cf
Manish Garg <manishgargpro@gmail.com>
2020-11-18 01:35:00 -0500

    vgg16 is working, albiet poorly

--- preproc.py
+++ preproc.py
@@ -1,18 +1,23 @@
 import csv
 import os
 import shutil
+import itertools
 from PIL import Image
 from pprint import pprint
 
 labelpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label.csv'
+testpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label_test.csv'
 imagepath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\origin'
-newpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\cropped'
+newpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\classified_test'
 labels = csv.DictReader(open(labelpath))
-names = []
-imagelist = []
+# names = []
+# confidences = []
+# classes = []
 
 # for l in labels:
-#     names.append(l["image_name"])
+#     # names.append(l["image_name"])
+#     if not in classes:
+#         classes.append(l["expression_label"])
 
 # for d, e, f in os.walk(imagepath):
 #     for fi in f:
@@ -22,8 +27,11 @@ imagelist = []
 #     if i not in names:
 #         shutil.copy2(os.path.join(imagepath, i), os.path.join(newpath, i))
 
-nameseen = []
-for l in labels:
+# nameseen = []
+
+
+def cropandsave(l):
+    # for l in labels:
     img = Image.open(os.path.join(imagepath, l["image_name"]))
     crop = img.crop((int(l["face_box_left"]),
                      int(l["face_box_top"]),
@@ -32,8 +40,28 @@ for l in labels:
                      ))
     newname = l["image_name"].rsplit(
         ".", 1)[0] + "_" + l["face_id_in_image"] + ".jpg"
-    # print(newname)
-    nameseen.append(newname)
-    crop.save(os.path.join(newpath, newname))
+    crop.save(os.path.join(newpath, l["expression_label"], newname))
 
-print(len(list(dict.fromkeys(nameseen))))
+
+num = 0
+w = csv.writer(open(testpath, "a", newline=""))
+for i in labels:
+    num += 1
+    # print(num)
+    if num % 10 == 0:
+        cropandsave(i)
+        # print(i)
+        # w.writerow([
+        #     i["image_name"],
+        #     i["face_id_in_image"],
+        #     i["face_box_top"],
+        #     i["face_box_left"],
+        #     i["face_box_right"],
+        #     i["face_box_bottom"],
+        #     i["face_box_confidence"],
+        #     i["expression_label"]
+        # ])
+
+print(num)
+
+# print(len(list(dict.fromkeys(nameseen))))
--- resnet.py
+++ resnet.py
@@ -5,13 +5,20 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
 import tensorflow as tf
+import pickle
 from keras.models import Sequential
-from keras.applications import ResNet50, ResNet50V2, ResNet101, ResNet101V2, ResNet152, ResNet152V2
+from keras.applications import DenseNet121, DenseNet169, DenseNet201
+from keras.applications.vgg16 import (
+    VGG16,
+    preprocess_input
+)
 from keras.layers import Conv2D, BatchNormalization, Dropout
 from keras.layers import AveragePooling2D
-from keras.layers import Dense, Flatten
-from keras.optimizers import SGD
+from keras.layers import Dense, Flatten, MaxPool2D
+from keras.models import Model
+from keras.optimizers import SGD, Adam
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import load_img, img_to_array
 # for conversion to one-hot coding
 from keras.utils import to_categorical
 # ======
@@ -19,29 +26,121 @@ from keras.utils import to_categorical
 # you have to make changes, if you are not using fashion_mnist dataset
 
 
-labelpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label.csv'
-imagepath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\resized_224'
-# destpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\'
-rows = csv.DictReader(open(labelpath))
-names = []
-confidences = []
-labels = []
-imagelist = []
+trainlabelpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label_filtered.csv'
+trainimgpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\cropped_train'
+testlabelpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\label\label_test.csv'
+testimgpath = r'C:\Users\Administrator\Desktop\Coding_stuff\facial_recognition\data\image\cropped_test'
+# rows = csv.DictReader(open(labelpath))
+# names = []
+# confidences = []
+# labels = []
+# imagelist = []
 
-for r in rows:
-    names.append(r["image_name"])
-    confidences.append(r["face_box_confidence"])
-    labels.append(r["expression_label"])
+# for r in rows:
+#     names.append(r["image_name"])
+#     confidences.append(r["face_box_confidence"])
+#     labels.append(r["expression_label"])
 
-def getimages():  
-  for d, e, f in os.walk(imagepath):
-      for fi in f:
-          fiarr = np.load(os.path.join(imagepath, fi))
-          imagelist.append(fiarr)
 
-def loadmodel():
-  model = Sequential()
-  model.add(ResNet50())
+def getimages():
+    Xtrain = ImageDataGenerator(rescale=1./255).flow_from_directory(
+        directory=trainimgpath,
+        target_size=(224, 224),
+        interpolation='bilinear'
+    )
+    Xtest = ImageDataGenerator(rescale=1./255).flow_from_directory(
+        directory=testimgpath,
+        target_size=(224, 224),
+        interpolation='bilinear'
+    )
+    # Ytrain = pd.read_csv(trainlabelpath, usecols=['expression_label'])
+    # Ytest = pd.read_csv(testlabelpath, usecols=['expression_label'])
+    return Xtrain, Xtest
 
-for i in imagelist:
-    print(i)
+
+def createvgg16():
+    # model = Sequential()
+    # model.add(Conv2D(input_shape=(224, 224, 3), filters=64,
+    #                  kernel_size=(3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=64, kernel_size=(3, 3),
+    #                  padding="same", activation="relu"))
+    # model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # model.add(Conv2D(filters=128, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=128, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # model.add(Conv2D(filters=256, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=256, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=256, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # model.add(Conv2D(filters=512, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=512, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=512, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # model.add(Conv2D(filters=512, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=512, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(Conv2D(filters=512, kernel_size=(
+    #     3, 3), padding="same", activation="relu"))
+    # model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    vggfeats = VGG16(weights='imagenet', include_top=False,
+                     input_shape=(224, 224, 3))
+    for layer in vggfeats.layers:
+        layer.trainable = False
+    model = Sequential()
+    model.add(vggfeats)
+    model.add(Flatten())
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dense(units=7, activation="softmax"))
+    return model
+
+
+def loadpretrained():
+    model = createvgg16()
+    Xtrain, Xtest = getimages()
+    # for l in model.layers:
+    #     print(l.output)
+    adam = Adam(learning_rate=0.01)
+    model.compile(optimizer=adam, loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.summary()
+    observations = model.fit(Xtrain, epochs=2,
+                             batch_size=1,
+                             validation_data=(Xtest), verbose=1)
+    acc = observations.history['accuracy']
+    val_acc = observations.history['val_accuracy']
+    loss = observations.history['loss']
+    val_loss = observations.history['val_loss']
+    epochs = range(len(acc))
+    plt.plot(epochs, acc, 'b', label="Training Accuracy")
+    plt.plot(epochs, loss, 'b', label="Training Loss")
+    plt.title("Training Accuracy and Loss")
+    plt.legend()
+    plt.figure()
+    plt.savefig("Training_Accuracy_and_Loss.png")
+    plt.plot(epochs, val_acc, 'b', label="Validation Accuracy")
+    plt.plot(epochs, val_loss, 'b', label="Validation Loss")
+    plt.title("Validation Accuracy and Loss")
+    plt.legend()
+    plt.figure()
+    plt.savefig("Validation_Accuracy_and_Loss.png")
+    # _, acc = model.evaluate(Xtest,  verbose=1)
+    # print('Final validation accuracy: %.3f' % (acc*100.0))
+    # model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+    # features = model.predict()
+    # print(features.shape)
+    # pickle.dump(features, open('test2.pkl', 'wb'))
+    # model.summary()
+
+
+loadpretrained()
+# getimages()
